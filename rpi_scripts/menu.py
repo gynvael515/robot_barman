90300a7c80ec96775d30d21c50aae973824c43cc
Damian Wypych <s165712@student.pg.edu.pl>
2020-06-01 18:09:29 +0100

    Corrected script for making drink and created class for menu

---
+++ rpi_scripts/menu.py
@@ -0,0 +1,33 @@
+import barman_classes
+
+
+
+class Menu:
+	def __init__(self):
+		self.drinks = {}
+		
+		# Drink1
+
+		name = "Screwdriver" 
+
+		ingredients = []
+		self.drinks[name]= barman_classes.Drink(name, ingredients)
+		# Drink2
+		# Drink3
+		# Drink4
+		# Drink5
+		# Drink6
+		# Drink7
+		# Drink8
+		# Drink9
+		# Drink10
+		# Drink11
+		# Drink12
+		# Drink13
+		# Drink14
+		# Drink15
+		# Drink16
+		# Drink17
+		# Drink18
+		# Drink19
+		# Drink20
--- rpi_scripts/robot_barman_new.py
+++ rpi_scripts/robot_barman_new.py
@@ -11,7 +11,7 @@ class Ingredient:
 
 
 	def __start_pwm(self):
-		GPIO.setup(self.pin, self.freq)
+		GPIO.setup(self.pin, GPIO.OUT)
 		self.pwm = GPIO.PWM(self.pin, self.freq)
 		return self.pwm
 
@@ -24,22 +24,24 @@ class Ingredient:
 
 
 class Drink:
-	def __init__(self, ingredients):
+	def __init__(self, name, ingredients):
+		self.name = name
 		self.ingredients = ingredients
-		self.sleep_time = 10
+		self.sleep_time = 3
 		self.pwms       = []
 
 
 
 	def make(self):
-		# Setup PWMs for each ingredient and store reference to PWMs in list
+		# Setup pins and PWMs for each ingredient and store reference to PWMs in list
 		for i in range(len(self.ingredients)):
-			self.pwms.append(GPIO.PWM(self.self.ingredients[i].pin, self.self.ingredients[i].freq))
+			GPIO.setup(self.ingredients[i].pin, GPIO.OUT)
+			self.pwms.append(GPIO.PWM(self.ingredients[i].pin, self.ingredients[i].freq))
 				
 				
 		# Run PWMs for each ingredient
 		for i in range(len(self.ingredients)):
-			self.pwms[i].start(self.self.ingredients[i].dc)
+			self.pwms[i].start(self.ingredients[i].dc)
 
 
 		# TODO: each alcohol should end in different time period
@@ -57,58 +59,26 @@ class Drink:
 
 
 
-class Drink_old:
-        def __init__(self, pins, dcs, freqs, sleep_time):
-                self.ingredients = []
-
-
-                self.pins               = pins
-                self.dcs                = dcs
-                self.freqs              = freqs
-                self.sleep_time = sleep_time
-                self.pwms               = []
-
-                for i in range(len(self.pins)):
-                        GPIO.setup(self.pins[i], GPIO.OUT)
-
-
-                for i in range(len(self.pins)):
-                        self.pwms.append(GPIO.PWM(self.pins[i], self.freqs[i]))
-
-
-        def make(self):
-                for i in range(len(self.pins)):
-                        self.pwms[i].start(self.dcs[i])
-
-
-                # TODO: each alcohol should end in different time period
-                time.sleep(self.sleep_time)
-
-                for i in range(len(self.pins)):
-                        GPIO.cleanup(self.pins[i])
-
-
-
 if __name__ == "__main__":
-        menu = {"vodka": 21, "tonic":4, "a":13}
+    menu = {"vodka": 21, "tonic":4, "a":13}
 
-		sleep_time = 5
-        ingredient0 = Ingredient(menu["tonic"], 100, 100, sleep_time)
-        ingredient1 = Ingredient(menu["vodka"], 50,  100, sleep_time)
-        ingredient2 = Ingredient(menu["a"],     100, 100, sleep_time)
-        ingredients = [ingredient0,
-                       ingredient1,
-                       ingredient2]
+	sleep_time = 5
+	ingredient0 = Ingredient(menu["tonic"], 100, 100, sleep_time)
+	ingredient1 = Ingredient(menu["vodka"], 50,  100, sleep_time)
+	ingredient2 = Ingredient(menu["a"],     100, 100, sleep_time)
+        
+	ingredients = [ingredient0,
+                   ingredient1,
+                   ingredient2]
 
 
-
-        GPIO.setmode(GPIO.BCM)
-        jonik = Drink(ingredients)
-        jonik.make()
-
-        exit()
-        jonik = Drink_old([menu["tonic"], menu["vodka"], 13], [100, 50, 100], [100, 100, 100],10)
-        jonik.make()
+	GPIO.setmode(GPIO.BCM)
+	jonik = Drink("drink", ingredients)
+	jonik.make()
+	
+	exit()
+	#jonik = Drink_old([menu["tonic"], menu["vodka"], 13], [100, 50, 100], [100, 100, 100],10)
+	#jonik.make()
 
 
 
