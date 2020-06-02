3375ce0fd4ad4a74ea1bfb7a66d202d532864d58
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2020-06-02 10:54:18 +0200

    .

--- rpi_scripts/menu.py
+++ rpi_scripts/menu.py
@@ -1,8 +1,60 @@
-import barman_classes
 from board import SCL, SDA
 import busio
 import time
 from adafruit_pca9685 import PCA9685
+
+
+class Ingredient:
+	def __init__(self, pin, dc, freq, sleep_time):
+		self.pin        = pin                   # pump
+		self.dc         = dc                    # duty cycle            \
+		self.freq       = freq                  # frequency             -      amount
+		self.sleep_time = sleep_time            # pumping time period   /
+
+
+	def __start_pwm(self):
+		GPIO.setup(self.pin, GPIO.OUT)
+		self.pwm = GPIO.PWM(self.pin, self.freq)
+		return self.pwm
+
+
+	def __clean_pin(self):
+		GPIO.cleanup(self.pin)
+
+
+
+
+
+class Drink:
+	def __init__(self, name, ingredients, pca):
+		self.name = name
+		self.ingredients = ingredients
+		self.sleep_time = 3
+		#self.pwms       = []
+		self.pca = pca
+
+
+	def __dc_percent2value(self, percent):
+		x = percent*64*1024/100
+		return x
+
+
+	def make(self):
+		# Run PWMs for each ingredient
+		for i in range(len(self.ingredients)):
+			pin = self.ingredients[i].pin
+			dc 	= self.ingredients[i].dc
+			self.pca.channels[pin].duty_cycle = self.__dc_percent2value(dc)
+
+
+		# TODO: each alcohol should end in different time period
+		time.sleep(self.sleep_time)
+
+
+		# Clean pin for each ingredient
+		for i in range(len(self.ingredients)):
+			pin = self.ingredients[i].pin
+			self.pca.channels[pin].duty_cycle = 0
 
 
 class Menu:
@@ -14,33 +66,34 @@ class Menu:
 		self.pca.frequency = 100
 
 		sleep_time = 5
-		menu = {"vodka": 0, "tonic":1, "a":2}
+		ingredients_dict = {"vodka": 0, "tonic":1, "a":2}
 
 		self.drinks = {}
 
 		# Drink1
 		name = "Screwdriver" 
-		ingredient0 = barman_classes.Ingredient(menu["tonic"], 100, 100, sleep_time)
-		ingredient1 = barman_classes.Ingredient(menu["vodka"], 50,  100, sleep_time)
-		ingredient2 = barman_classes.Ingredient(menu["a"],     100, 100, sleep_time)
+		ingredient0 = Ingredient(ingredients_dict["tonic"], 100, 100, sleep_time)
+		ingredient1 = Ingredient(ingredients_dict["vodka"], 50,  100, sleep_time)
+		ingredient2 = Ingredient(ingredients_dict["a"],     100, 100, sleep_time)
 		ingredients = [ingredient0, ingredient1, ingredient2]
-		self.drinks[name]= barman_classes.Drink(name, ingredients, self.pca)
+		self.drinks[name]= Drink(name, ingredients, self.pca)
 
 		# Drink2
 		name = "TurboCola" 
-		ingredient0 = barman_classes.Ingredient(menu["tonic"], 100, 100, sleep_time)
-		ingredient1 = barman_classes.Ingredient(menu["vodka"], 50,  100, sleep_time)
-		ingredient2 = barman_classes.Ingredient(menu["a"],     100, 100, sleep_time)
+		ingredient0 = Ingredient(ingredients_dict["tonic"], 100, 100, sleep_time)
+		ingredient1 = Ingredient(ingredients_dict["vodka"], 50,  100, sleep_time)
+		ingredient2 = Ingredient(ingredients_dict["a"],     100, 100, sleep_time)
 		ingredients = [ingredient0, ingredient1, ingredient2]
-		self.drinks[name]= barman_classes.Drink(name, ingredients, self.pca)
+		self.drinks[name]= Drink(name, ingredients, self.pca)
 		
 		# Drink3
 		name = "Drink1" 
-		ingredient0 = barman_classes.Ingredient(menu["tonic"], 100, 100, sleep_time)
-		ingredient1 = barman_classes.Ingredient(menu["vodka"], 50,  100, sleep_time)
-		ingredient2 = barman_classes.Ingredient(menu["a"],     100, 100, sleep_time)
+		ingredient0 = Ingredient(ingredients_dict["tonic"], 100, 100, sleep_time)
+		ingredient1 = Ingredient(ingredients_dict["vodka"], 50,  100, sleep_time)
+		ingredient2 = Ingredient(ingredients_dict["a"],     100, 100, sleep_time)
 		ingredients = [ingredient0, ingredient1, ingredient2]
-		self.drinks[name]= barman_classes.Drink(name, ingredients, self.pca)
+		self.drinks[name]= Drink(name, ingredients, self.pca)
+		
 		# Drink4
 		# Drink5
 		# Drink6
