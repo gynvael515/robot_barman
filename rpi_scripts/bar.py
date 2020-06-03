67afb78e7c587351ab7f25cad73f98aec89869b3
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2020-06-03 21:13:15 +0200

    .

---
+++ rpi_scripts/bar.py
@@ -0,0 +1,38 @@
+from kivy.app import App 
+from kivy.uix.gridlayout import GridLayout 
+from kivy.uix.FloatLayout import FloatLayout 
+from kivy.uix.RelativeLayout import RelativeLayout 
+from kivy.uix.label import Label 
+from kivy.uix.textinput import TextInput 
+  
+  
+class Barman_gui(RelativeLayout): 
+    def __init__(self, **var_args): 
+          
+        super(Barman_gui, self).__init__(**var_args) 
+        
+        Buttony = []
+        Obrazki = []
+ 
+        Buttony.append(Button(text='Drin 1' ,size_hint=(.15,.1) ,pos_hint={"center_x":.2,"center_y":.2})) #on_press=self.clearText --> KRZYCHOWA FUNKCJA
+        Buttony.append(Button(text='Drin 2' ,size_hint=(.15,.1) ,pos_hint={"center_x":.5,"center_y":.2})) #on_press=self.clearText --> KRZYCHOWA FUNKCJA
+        Buttony.append(Button(text='Drin 3' ,size_hint=(.15,.1) ,pos_hint={"center_x":.8,"center_y":.2})) #on_press=self.clearText --> KRZYCHOWA FUNKCJA
+        
+        Obrazki.append(Image(source='1.png'), size_hint=(.1,.2))
+        Obrazki.append(Image(source='2.png'), size_hint=(.1,.2))
+        Obrazki.append(Image(source='3.png'), size_hint=(.1,.2))
+ 
+        for i in range(len(Buttony)):
+                self.add_widget(Buttony[i])
+                self.add_widget(Obrazki[i])
+ 
+  
+ 
+class Bar(App):  
+    def build(self): 
+        # return a Barman_gui() as a root widget 
+        return Barman_gui() 
+  
+  
+if __name__ == '__main__': 
+    Bar().run() 
