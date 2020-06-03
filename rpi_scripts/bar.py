a97ca696bf7c2a87fbf56ae3265a36960e54e024
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2020-06-03 20:37:26 +0100

    .

--- rpi_scripts/bar.py
+++ rpi_scripts/bar.py
@@ -9,24 +9,29 @@ from kivy.uix.image import Image
 
   
 class Barman_gui(RelativeLayout): 
-    def __init__(self, **var_args): 
-          
-        super(Barman_gui, self).__init__(**var_args) 
+	def __init__(self, **var_args): 
+		path = 'src/'
+		super(Barman_gui, self).__init__(**var_args) 
         
-        Buttony = []
-        Obrazki = []
+		Buttony = []
+		Obrazki = []
  
-        Buttony.append(Button(text='Drin 1' ,size_hint=(.15,.1) ,pos_hint={"center_x":.2,"center_y":.2})) #on_press=self.clearText --> KRZYCHOWA FUNKCJA
-        Buttony.append(Button(text='Drin 2' ,size_hint=(.15,.1) ,pos_hint={"center_x":.5,"center_y":.2})) #on_press=self.clearText --> KRZYCHOWA FUNKCJA
-        Buttony.append(Button(text='Drin 3' ,size_hint=(.15,.1) ,pos_hint={"center_x":.8,"center_y":.2})) #on_press=self.clearText --> KRZYCHOWA FUNKCJA
-        
-        Obrazki.append(Image(source='1.png'), size_hint=(.1,.2))
-        Obrazki.append(Image(source='2.png'), size_hint=(.1,.2))
-        Obrazki.append(Image(source='3.png'), size_hint=(.1,.2))
+		Buttony.append(Button(text='Drin 1' ,size_hint=(.15,.1) ,pos_hint={"center_x":.2,"center_y":.2})) #on_press=self.clearText --> KRZYCHOWA FUNKCJA
+		Buttony.append(Button(text='Drin 2' ,size_hint=(.15,.1) ,pos_hint={"center_x":.5,"center_y":.2})) #on_press=self.clearText --> KRZYCHOWA FUNKCJA
+		Buttony.append(Button(text='Drin 3' ,size_hint=(.15,.1) ,pos_hint={"center_x":.8,"center_y":.2})) #on_press=self.clearText --> KRZYCHOWA FUNKCJA
+
+		Obrazki.append(Image(source=path+'1.jpg', size_hint=(.1,.2)))
+		Obrazki.append(Image(source=path+'2.png', size_hint=(.1,.2)))
+		Obrazki.append(Image(source=path+'3.png', size_hint=(.1,.2)))
+
+
+		#	Obrazki.append(Image(source=path+'2.png', size_hint=(.1,.2))
+    	#   Obrazki.append(Image(source=path+'3.png', size_hint=(.1,.2))
  
-        for i in range(len(Buttony)):
-                self.add_widget(Buttony[i])
-                self.add_widget(Obrazki[i])
+
+		for i in range(len(Buttony)):
+			self.add_widget(Buttony[i])
+			self.add_widget(Obrazki[i])
  
   
  
@@ -37,8 +42,5 @@ class Bar(App):
   
   
 if __name__ == '__main__': 
-<<<<<<< HEAD
-    Bar().run() 
-=======
-    Bar().run() 
->>>>>>> a2af71e8102e924e8d10d13d977d9c6f8fe9b16c
+	Bar().run() 
+
